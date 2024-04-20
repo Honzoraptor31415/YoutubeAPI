7a4377f40372d8bb0083327c30468411264cf9c5
Honzoraptor <j.ackermann@email.cz>
2024-04-20 12:38:18 +0200

    URL is now provided as a URL param and also I added some error handling

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-from flask import Flask
+from flask import Flask, request
 from pytube import YouTube
 
-# just an example
-author = YouTube("https://www.youtube.com/watch?v=mo1TN-quILs").author
+# example video id: Up5Gm_Ls2oQ
 
-def get_data(id: str):
-  return {
-    "title": YouTube(f"https://www.youtube.com/watch?v={id}").title,
-    "author": YouTube(f"https://www.youtube.com/watch?v={id}").author,
-    "thumbnail_url": YouTube(f"https://www.youtube.com/watch?v={id}").thumbnail_url,
-    "channel_url": YouTube(f"https://www.youtube.com/watch?v={id}").channel_url,
-    "length_seconds": YouTube(f"https://www.youtube.com/watch?v={id}").length
-  }
+def get_data(url: str):
+  try:
+    ytd = YouTube(url)
+    print(url)
+    return {
+      "title": ytd.title,
+      "author": ytd.author,
+      "thumbnail_url": ytd.thumbnail_url,
+      "channel_url": ytd.channel_url,
+      "length_seconds": ytd.length,
+      "keywords": ytd.keywords,
+      "publish_data": ytd.publish_date,
+      "views": ytd.views
+    }
+  except:
+    return {
+      "message": "Invalid URL"
+    }
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+  url = request.args.get("url")
+  if url:
+    return get_data(url)
+  else:
+    return {
+      "message": "Please provide a valid URL in the URL params"
+    }
+
 @app.route("/<id>")
-def index(id):
+def vid_id_route(id):
   return get_data(id)
