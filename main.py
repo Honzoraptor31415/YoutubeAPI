994b4ced42c0aa45fd93efa163c9d0f87f5b8142
Honzoraptor <j.ackermann@email.cz>
2024-04-20 16:00:14 +0200

    Added more error handling and getting different data based on the url
    That means that if it's a playlist URL, it's gonna return a playlist data and same for video and channel

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from flask import Flask, request, redirect
 from pytube import YouTube
+import re
 
 # example video id: Up5Gm_Ls2oQ
 
+invalid_url_msg = "Please provide a valid URL"
+
 def get_data(url: str):
+  try:
+    dot_split = url.split(".")
+    dot_split_last = dot_split[len(dot_split)-1]
+    url_route_1stchar = dot_split_last.split("/")[1][0]
+    url_route = dot_split_last.split("/")[1].split("?")[0]
+    
+    if url_route_1stchar == "@":
+      return get_channel_data(url)
+    elif url_route == "watch":
+      return get_vid_data(url)
+    elif url_route == "playlist":
+      return get_playlist_data(url)
+    else:
+      return {
+        "message": invalid_url_msg
+      }
+    
+  except:
+    return {
+      "message": invalid_url_msg
+    }
+    
+  
+def get_vid_data(url: str):
   try:
     ytd = YouTube(url)
     print(url)
     return {
+      "type": "video",
       "title": ytd.title,
       "author": ytd.author,
       "thumbnail_url": ytd.thumbnail_url,
@@ -19,8 +47,28 @@ def get_data(url: str):
     }
   except:
     return {
-      "message": "Invalid URL"
+      "message": invalid_url_msg
     }
+
+def get_channel_data(url: str):
+  # ytd = YouTube(url)
+  # print(url)
+  return {
+    "type": "channel"
+    # "title": ytd.title,
+    # "author": ytd.author,
+    # "thumbnail_url": ytd.thumbnail_url,
+    # "channel_url": ytd.channel_url,
+    # "length_seconds": ytd.length,
+    # "keywords": ytd.keywords,
+    # "publish_data": ytd.publish_date,
+    # "views": ytd.views
+  }
+
+def get_playlist_data(url: str):
+  return {
+    "type": "playlist"
+  }
 
 app = Flask(__name__)
 
@@ -31,7 +79,7 @@ def index():
     return get_data(url)
   else:
     return {
-      "message": "Please provide a valid URL in the URL params."
+      "message": invalid_url_msg
     }
   
 @app.route("/github")
